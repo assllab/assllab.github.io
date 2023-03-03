930ab29b8889555345986f124195ea634ba33672
HYASSLLAB@gmail.com <HYASSLLAB@gmail.com>
2023-03-03 10:16:32 +0900

    delete

--- client/src/pages/membersPage/MemberPage.js
+++ client/src/pages/membersPage/MemberPage.js
@@ -8,7 +8,6 @@ import EducationList from "../../components/EducationList/EducationList";
 import ArticleList from "../../components/articleList/ArticleList";
 import BorderLine from "../../components/borderLine/borderLine";
 import hoonJungImg from "../../images/membersPage/HoonJung.jpg"
-import JongSeonParkImg from "../../images/membersPage/JongSeonPark.jpg"
 import YeJinOhImg from "../../images/membersPage/YeJinOh.png"
 import InChulParkImg from "../../images/membersPage/InChulPark.png"
 import EunKyoungParkImg from "../../images/membersPage/EunKyoungPark.png"
@@ -21,7 +20,7 @@ import ChaeWonJeongImg from "../../images/membersPage/ChaeWonJeong.jpg"
 
 function MemberPage() {
     const members = [
-        {name: "Jong Seon Park", position: "Ph. D. student", desc: "jonsn.park◾gmail.com", image: JongSeonParkImg},
+        
         {name: "Ye Jin Oh", position: "Ph. D. student", desc: "ohyejin0730◾gmail.com", image: YeJinOhImg},
         {name: "In Chul Park", position: "Master student", desc: "parkinchul601◾gmail.com", image: InChulParkImg},
         {name: "Eun Kyoung Park", position: "Master student", desc: "eunkyoung120◾hanyang.ac.kr", image: EunKyoungParkImg},
