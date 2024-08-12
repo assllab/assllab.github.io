c479396abbe833e12944aa23eff2af73076a1b5d
HYASSLLAB@gmail.com <HYASSLLAB@gmail.com>
2024-08-12 14:51:21 +0900

    revise

--- client/src/pages/membersPage/MemberPage.js
+++ client/src/pages/membersPage/MemberPage.js
@@ -8,32 +8,26 @@ import EducationList from "../../components/EducationList/EducationList";
 import ArticleList from "../../components/articleList/ArticleList";
 import BorderLine from "../../components/borderLine/borderLine";
 import hoonJungImg from "../../images/membersPage/HoonJung.jpg"
-import YeJinOhImg from "../../images/membersPage/YeJinOh.png"
-import InChulParkImg from "../../images/membersPage/InChulPark.png"
 import EunKyoungParkImg from "../../images/membersPage/EunKyoungPark.png"
+import InChulParkImg from "../../images/membersPage/InChulPark.png"
 import ShinJuYongImg from "../../images/membersPage/Shinjuyong.jpg"
 import HaeNyLeeImg from "../../images/membersPage/HaenyLee.jpg"
 import SungMokKimImg from "../../images/membersPage/SungMokKim.jpg"
 import HyeRanLeeImg from "../../images/membersPage/HyeRanLee.jpg"
 import MoonSeockKoImg from "../../images/membersPage/MoonSeockKo.png"
-import ChaeWonJeongImg from "../../images/membersPage/ChaeWonJeong.jpg"
 import JunBeomParkImg from "../../images/membersPage/JunBeomPark.jpg"
-import JeongWonOhImg from "../../images/membersPage/JeongWonOh.jpg"
 
 function MemberPage() {
     const members = [
         
-        {name: "Ye Jin Oh", position: "Ph. D. student", desc: "ohyejin0730◾gmail.com", image: YeJinOhImg},
-        {name: "In Chul Park", position: "Master student", desc: "parkinchul601◾gmail.com", image: InChulParkImg},
         {name: "Eun Kyoung Park", position: "Master student", desc: "eunkyoung120◾hanyang.ac.kr", image: EunKyoungParkImg},
+        {name: "In Chul Park", position: "Master student", desc: "parkinchul601◾gmail.com", image: InChulParkImg},
         {name: "Ju Yong Shin", position: "Master Student", desc: "balance0415◾hanyang.ac.kr", image: ShinJuYongImg},
         {name: "Hae Ny Lee", position: "Master Student", desc: "haeny6060◾hanyang.ac.kr", image: HaeNyLeeImg},
         {name: "Sung Mok Kim", position: "Master Student", desc: "sungmok06◾hanyang.ac.kr", image: SungMokKimImg},
         {name: "Hye Ran Lee", position: "Master Student", desc: "hyeran245◾hanyang.ac.kr", image: HyeRanLeeImg},
         {name: "Moon Seock Ko", position: "Master Student", desc: "komoonseock◾hanyang.ac.kr", image: MoonSeockKoImg},
-        {name: "Chae Won Jeong", position: "Undergraduate Student", desc: "chaewon1◾hanyang.ac.kr", image: ChaeWonJeongImg},
-        {name: "Jun Beom Park", position: "Undergraduate Student", desc: "yrtny70127◾gmail.com", image: JunBeomParkImg},
-        {name: "Jeong Won Oh", position: "Undergraduate Student", desc: "okojw0◾hanyang.ac.kr", image: JeongWonOhImg},
+        {name: "Jun Beom Park", position: "Master Student", desc: "yrtny70127◾gmail.com", image: JunBeomParkImg},
 
     ]
     return (
